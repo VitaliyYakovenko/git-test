0e2de162fbb69665a4eda6c7141f9b76c1b17bfb
VitaliyYakovenko <yakovenkovitaliy1@gmail.com>
2025-03-27 16:08:23 +0200

    add qwe

--- js/main.js
+++ js/main.js
@@ -1,26 +1,5 @@
-function isIsogram(str){
-    if (str === "" || str.length === 1) {
-        return true
-    }
-
-    const arr = str.toLowerCase().split("");
- 
-    const results = arr.sort((a,b ) => a.localeCompare(b))
-    const value = results[0];
-    console.log(results)
-    
-    for (let i = 1; i < results.length; i += 1){
-        if (value === results[i]) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-  
-    
-
-}
 
 
+const a = "hello world"
+console.log(a);
 
-console.log(isIsogram("tblmvnuwxsthqr"))
